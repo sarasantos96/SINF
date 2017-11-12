f8ab072a2eafd7746723da642fa5a44cb14fdb74
Ana Rita Torres <anaritatorres96@outlook.pt>
2017-11-12 22:27:46 +0000

    product page started

---
+++ Controllers/CatalogoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FirstREST.Lib_Primavera.Model;
+
+namespace FirstREST.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
