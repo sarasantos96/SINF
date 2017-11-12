a0e502abfe4ad2334f52a34b0c9e35e67091bf97
Unknown <nuno.freitas96@gmail.com>
2017-11-12 17:36:38 +0000

    Start of Admin View

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstREST.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ArtigosStock()
+        {
+            ViewBag.listaArtigos = Lib_Primavera.PriIntegration.ListaArtigos();
+            return View();
+        }
+
+    }
+}
