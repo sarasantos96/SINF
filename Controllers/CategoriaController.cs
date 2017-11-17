5d5e9e948d6c60d7e6a426416a297803bb15c511
Sara Santos <santos.sara96@gmail.com>
2017-11-17 10:03:11 +0000

    Deleted unused code

--- Controllers/CategoriaController.cs
+++ Controllers/CategoriaController.cs
@@ -28,28 +28,7 @@ namespace FirstREST.Controllers
             ViewBag.categoria = Lib_Primavera.PriIntegration.GetCategoria(id);
             return View();
         }
-        /*
-        //
-        // GET: /Categoria/
-        public IEnumerable<Lib_Primavera.Model.Categoria> Get()
-        {
-            return Lib_Primavera.PriIntegration.ListaCategorias();
-        }
 
-        // GET api/Categoria/5    
-        public Categoria Get(string id)
-        {
-            Lib_Primavera.Model.Categoria categoria = Lib_Primavera.PriIntegration.GetCategoria(id);
-            if (categoria == null)
-            {
-                throw new HttpResponseException(
-                  Request.CreateResponse(HttpStatusCode.NotFound));
-            }
-            else
-            {
-                return categoria;
-            }
-        }*/
 
     }
 }
